c6b6627a585be838fda276452476c7492e39f3fd
Andre Fonseca <oandrefonseca@gmail.com>
2024-05-06 12:18:18 -0500

    Adding check metadata

--- .cirro/align/preprocess.py
+++ .cirro/align/preprocess.py
@@ -4,26 +4,10 @@ from cirro.helpers.preprocess_dataset import PreprocessDataset
 import pandas as pd
 import os
 
-def adding_modality():
-    pass
-
-def setup_input_parameters(ds: PreprocessDataset):
-    # If the user did not select a custom malignant table, use the default
-    pass
-
-if __name__ == "__main__":
-
-    ds = PreprocessDataset.from_running()
-    setup_input_parameters(ds)
-
-    ds.logger.info("Files annotated in the dataset:")
-    ds.logger.info(ds.files)
-
-    ds.logger.info("Printing metadata related to dataset:")
-    ds.logger.info(ds.samplesheet)
-
+def adding_modality(ds: PreprocessDataset) -> pd.DataFrame:):
+    
     # Make a wide sample_table
-    ds.logger.info("Pivoting to wide format:")
+    ds.logger.info("Adding modality column from:")
     sample_table = ds.wide_samplesheet(
         index=["sampleIndex", "sample", "lane"],
         columns="read",
@@ -32,7 +16,16 @@ if __name__ == "__main__":
     ).sort_values(
         by="sample"
     )
-    ds.logger.info(sample_table.to_csv(index=None))
+
+if __name__ == "__main__":
+
+    ds = PreprocessDataset.from_running()
+
+    ds.logger.info("Files annotated in the dataset:")
+    ds.logger.info(ds.files)
+
+    ds.logger.info("Checking medata:")
+    ds.logger.info(ds.ds.samplesheet)
 
     ds.logger.info("Exported paths:")
     ds.logger.info(os.environ['PATH'])
