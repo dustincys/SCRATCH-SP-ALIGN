e89c546431f45931a2b720be5077151baaa67d7d
Andre Fonseca <oandrefonseca@gmail.com>
2024-05-06 11:52:36 -0500

    Multiple prints for debugging

--- .cirro/align/preprocess.py
+++ .cirro/align/preprocess.py
@@ -16,8 +16,20 @@ if __name__ == "__main__":
     ds = PreprocessDataset.from_running()
     setup_input_parameters(ds)
 
-    ds.logger.info("Standard Cirro's samplesheet:")
-    ds.logger.info(ds.wide_samplesheet.to_csv(index=None))
+    ds.logger.info("Files annotated in the dataset:")
+    ds.logger.info(ds.files)
+
+    # Make a wide sample_table
+    ds.logger.info("Pivoting to wide format")
+    sample_table = ds.wide_samplesheet(
+        index=["sampleIndex", "sample", "lane"],
+        columns="read",
+        values="file",
+        column_prefix="fastq_"
+    ).sort_values(
+        by="sample"
+    )
+    ds.logger.info(sample_table.to_csv(index=None))
 
     ds.logger.info("Exported paths:")
     ds.logger.info(os.environ['PATH'])
