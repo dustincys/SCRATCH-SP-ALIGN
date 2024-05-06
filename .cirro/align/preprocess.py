9502316d27c78710d636ddd6d6f11e1d1ff5cdc6
Andre Fonseca <oandrefonseca@gmail.com>
2024-05-06 12:22:45 -0500

    Minor mistaken

--- .cirro/align/preprocess.py
+++ .cirro/align/preprocess.py
@@ -4,10 +4,12 @@ from cirro.helpers.preprocess_dataset import PreprocessDataset
 import pandas as pd
 import os
 
-def adding_modality(ds: PreprocessDataset) -> pd.DataFrame:):
+def adding_modality(ds: PreprocessDataset) -> pd.DataFrame):
     
     # Make a wide sample_table
     ds.logger.info("Adding modality column from:")
+
+    # Can I check if has modality column?
     sample_table = ds.wide_samplesheet(
         index=["sampleIndex", "sample", "lane"],
         columns="read",
@@ -16,6 +18,10 @@ def adding_modality(ds: PreprocessDataset) -> pd.DataFrame:):
     ).sort_values(
         by="sample"
     )
+
+    # How can I read the metadata? Is it ds.samplesheet?
+    # sample_table = sample_table.merge(ds.samplesheet)
+    # return sample_table
 
 if __name__ == "__main__":
 
